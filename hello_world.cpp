ab144003b0de43ccaf73b9e188ec15c9f0ef4063
Pawel <66121057+pawelkolimaga@users.noreply.github.com>
2023-06-08 13:54:17 +0200

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<string>
+
+inline void keep_window_open();
+
+int main(){
+
+    std::cout << "Hello world, it's me xD\nHello Programmers\nLet's get started!\n\n\n";
+
+}
+
+// hello, useless function
+
+inline void keep_window_open(){
+    char ch;
+    std::cin >> ch;
+}
