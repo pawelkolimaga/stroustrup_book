62634145855a22cabf4469374170235275c7b473
Pawel Kolimaga <kolino3@gmail.com>
2023-06-09 14:39:37 +0200

    Adding new files for chapter 3

---
+++ directives_library.hpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<string>
---
+++ object_types_values.cpp
@@ -0,0 +1,10 @@
+#include "directives_library.hpp"
+
+int main(){
+
+    std::cout << "Provide your name and press enter\n\n";
+    std::string first_name;
+    std::cin >> first_name;
+    std::cout << "Hello, " << first_name << "!\n\n";
+
+}
