514f2103523e7c2fe147a6beda79e98978628088
Pawel Kolimaga <kolino3@gmail.com>
2023-06-28 14:58:12 +0200

    letter beginning

--- object_types_values.cpp
+++ object_types_values.cpp
@@ -9,25 +9,45 @@ bool isCorrect (double &age){
     return false;
 }
 
+std::string Letter (std::string &str){
+    return str;
+}
+
 int main(){
 
-    std::cout << "Provide your name and age and press enter\n\n";
-    double age = 0;
+    int a = 'A';
+    std::cout << "\n" 
+        << a 
+        << "\n\n";
+
+    bool b = true;
+    char c = b;
+    std::cout << "\n" 
+        << c 
+        << "\n\n";
+
     std::string first_name;
     std::string second_name;
+    std::string receiver_name;
+    double age = 0;
+
+    std::cout << "Provide your name and age and press enter\n\n";
     std::cin >> first_name >> second_name;
+
     std::string full_name = first_name + ' ' + second_name;
-
+    
     do{
-    std::cout << "OK! Now please give me your accurate age: ";
-    std::cin >> age;
-
+        std::cout << "OK! Now please give me your accurate age: ";
+        std::cin >> age;
     }while(isCorrect(age));
     
-    std::cout << "\n\nHello, " << full_name << "!\n\n";
-    std::cout << "And your age is " << age*12 << " months.\n\n";
+    std::cout << "\nTo whom do you want to write? ";
+    std::cin >> receiver_name;
     
-    std::cout << sqrt(8) << "\n\n\n";
+    //std::cout << "\n\nHello, " << full_name << "!\n\n";
+    //std::cout << "And your age is " << age*12 << " months.\n\n";
+    
+    //std::cout << sqrt(8) << "\n\n\n";
 
     std::string current;
     std::string previous = " ";
@@ -37,14 +57,13 @@ int main(){
         std::cin >> current;
         number_of_words++;
         if(previous==current){
-            std::cout << "Repeatint words: " << current << "\n\n\n";
-            
-
-        previous = current;    
+            std::cout << "Repeatint words: " << current << "\n\n\n";    
         }
+        previous = current;
         //if(first_name == "exit"){
         //    break;
         //}
     }
+    std::cout << "\n\nNumber of words: " << number_of_words << "\n\n";
 
 }
