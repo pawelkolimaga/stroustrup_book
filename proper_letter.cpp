e0aea03a43328747f86328d9096e65e0bffa02c1
Pawel Kolimaga <kolino3@gmail.com>
2023-08-28 11:53:32 +0200

    some_classes

--- proper_letter.cpp
+++ proper_letter.cpp
@@ -1,9 +1,17 @@
 #include "directives_library.hpp"
 #include "letter_classes.hpp"
+    
+int main()
+{
+    std::cout << "hello\n\n";
 
-int main(){
+    Letter_User Pawel("Pawel","Koli",3);
+    Letter_User Kuba;
+    std::cout << Pawel.get_lastName() << "\n\n";
+    Kuba.set_lastName("Kuba");
+    Pawel.set_lastName("Podpora");
+    std::cout << Kuba.get_lastName() << "\n\n";
 
-    say_hello hello;
-    std::cout << hello.s << "\n\n";
-
+    Monitor monitor_apple("jan");
+    
 }
