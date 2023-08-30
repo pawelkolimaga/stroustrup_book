1132a1451c8c7def1bd7e6fc039308a17890f53e
Pawel Kolimaga <kolino3@gmail.com>
2023-08-30 16:45:19 +0200

    vector task added

--- proper_letter.cpp
+++ proper_letter.cpp
@@ -3,15 +3,27 @@
     
 int main()
 {
-    std::cout << "hello\n\n";
+    int l = 0x234FF;
+    unsigned j = 04566332;
+    std::string ll = std::to_string(l);
 
-    Letter_User Pawel("Pawel","Koli",3);
-    Letter_User Kuba;
-    std::cout << Pawel.get_lastName() << "\n\n";
-    Kuba.set_lastName("Kuba");
-    Pawel.set_lastName("Podpora");
-    std::cout << Kuba.get_lastName() << "\n\n";
+    Ship Black_Pearl; 
+    Black_Pearl += 5;
+    std::cout << Black_Pearl.getId() << std::endl;
+    std::cout << Black_Pearl.getName() << std::endl;
+    std::cout << Black_Pearl.getSpeed() << std::endl;
+    std::cout << Black_Pearl.getMaxCrew() << std::endl;
+    std::cout << Black_Pearl.getCapacity() << std::endl;
 
-    Monitor monitor_apple("jan");
-    
+    std::vector<int> vec1 = {1,2,3,4,5};
+    std::vector<int> vec2 = {20,564};
+
+    std::vector<int> vec;
+
+    vec = concatenateVectors(vec1, vec2);
+ 
+    for (auto it : vec){
+    std::cout << it << "\n";
+    }
+
 }
